ca1b487e4acf9239a2e641d1652203fb54c4f852
ChanYoungHan <sanbon0812@naver.com>
2021-12-17 01:19:10 +0900

    docker run shell script 조건문 추가

--- docker_run_mount_vis.sh
+++ docker_run_mount_vis.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <image> <name>" >&2
+#!/bin/bash
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <image> <name> <mode>" >&2
+
+  echo "Now available mode : \n\t default"
   exit 1
 fi
 
-docker run -itd -v /tmp/.X11-unix/:/tmp/.X11-unix -e DISPLAY=unix:1 --gpus all --mount type=bind,source=/media/hcy/0f492418-c958-478b-84b9-b50a1c6103f1/data,target=/data --name $2 $1
+echo "docker with <mode> = $3"
+
+if [ $3 = "default" ]; then
+  docker run -itd -v /tmp/.X11-unix/:/tmp/.X11-unix -e DISPLAY=unix:1 --gpus all --mount type=bind,source=/media/hcy/0f492418-c958-478b-84b9-b50a1c6103f1/data,target=/data --name $2 $1
+
+elif [ $3 = "no-display" ]; then
+  docker run -itd -v /tmp/.X11-unix/:/tmp/.X11-unix -e DISPLAY=unix:1 --gpus all --mount type=bind,source=/media/hcy/0f492418-c958-478b-84b9-b50a1c6103f1/data,target=/data --name $2 $1
+
+else
+  echo "No option to run"
+  echo "To run docker with all option, set <mode>=default"
+  exit 1
+fi
+
+return 0
+
 
